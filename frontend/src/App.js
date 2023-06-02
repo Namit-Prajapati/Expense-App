f4d1a5827767cd8b4b827eabb01fea2058500bab
Namit-Prajapati <namitprajapati51@gmail.com>
2023-06-02 10:42:46 +0530

    login routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import { ExpensePage } from "./components/pages/ExpensePage";
 import Login from "./components/pages/Login";
 
 const App = () => {
+  const user = true;
   const router = createBrowserRouter([
     {
       path: "/",
@@ -11,7 +12,9 @@ const App = () => {
     },
     {
       path: "/expenses",
-      element: <ExpensePage />,
+      element:<>
+       {user ?<ExpensePage /> : <Navigate to="/" replace={true}/>}
+      </>, 
     },
   ]);
 
