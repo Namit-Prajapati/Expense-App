33eca25994331378152d394614ce7ec460407def
Namit-Prajapati <namitprajapati51@gmail.com>
2023-06-05 10:57:55 +0530

    auth routes

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -1,7 +1,20 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
-exports.login = (req, res, next) => {};
+exports.login = async (req, res, next) => {
+  const email = req.body.email;
+  const pass = req.body.password;
+
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    return res.status(404).json({ message: "user not found" });
+  }
+  const isEqual = await bcrypt.compare(pass, user.password);
+  if (!isEqual) {
+    return res.status(402).json({ message: "wrong credentials" });
+  }
+  res.status(200).json({ message: "login successful!", user: user });
+};
 
 exports.signup = async (req, res, next) => {
   const email = req.body.email;
